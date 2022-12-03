b1ec8d83c125ecc2fc8609a9f2de62704dce5df1
Luke Faupel <lukefaupel@gmail.com>
2022-12-03 11:59:38 -0600

    updated cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,10 @@
+/* eslint-disable max-len */
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const FieldValue = require("firebase-admin").firestore.FieldValue;
+admin.initializeApp();
+
+const db = admin.firestore();
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
@@ -9,28 +15,51 @@ const functions = require("firebase-functions");
 // });
 
 exports.updateList = functions.firestore
-  .document('users/{id}')
+  .document("users/{id}")
   .onWrite((change, context) => {
     const newValue = change.after.data();
-
     const previousValue = change.before.data();
-    
-    //See if user is checking in
-    if(newValue.isCheckedIn == true && previousValue.isCheckedIn == false) {
-      //User is checked in
-      //Check user pickup location 
-      switch(pickupLocation) {
+
+    const pickupLocation = newValue.pickupLocation;
+
+    const activePickupsRef = db.doc("ActivePickups/main");
+
+    // See if user is checking in
+    if (newValue.isCheckedIn === true && previousValue.isCheckedIn === false) {
+      // User is checked in
+      // Check user pickup location
+      switch (pickupLocation) {
         case "Playground":
           // add id to playground map
-          
+          activePickupsRef.update({
+            Playground: FieldValue.arrayUnion(context.params.id),
+          });
+
           break;
         case "Art Wing":
-          //add id to art wing map
+          activePickupsRef.update({
+            ArtWing: FieldValue.arrayUnion(context.params.id),
+          });
           break;
       }
-    
-    } else if(newValue == 'false' && previousValue == 'true') {
-      //User is checked out
-      //Remove id from list
+    } else if (
+      newValue.isCheckedIn === false &&
+      previousValue.isCheckedIn === true
+    ) {
+      // User is checked out
+      // Remove id from list
+      switch (pickupLocation) {
+        case "Playground":
+          // add id to playground map
+          activePickupsRef.update({
+            Playground: FieldValue.arrayRemove(context.params.id),
+          });
+          break;
+        case "Art Wing":
+          activePickupsRef.update({
+            ArtWing: FieldValue.arrayRemove(context.params.id),
+          });
+          break;
+      }
     }
- });
+  });
