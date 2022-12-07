ae98918c5e42ae894e6d5428a1eab7fcab18d212
Jibaspoudel <94658376+Jibaspoudel@users.noreply.github.com>
2022-12-07 16:50:42 -0600

    added realtime update

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,7 @@ function App() {
     const [users, setUsers] = useState([]);
     const usersCollectionRef = collection(db, "users");
     useEffect(() => {
-     onSnapshot(usersCollectionRef,limit(1),(snapshot)=>{
+     onSnapshot(usersCollectionRef,(snapshot)=>{
       const getUsers = async () => {
         const data = await getDocs(usersCollectionRef);
         setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
